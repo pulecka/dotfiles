5994c77a9bb13faa18be8ae1c99d5edb88ceba32
Martin Obert <me@pulecka.com>
2020-03-16 13:43:24 +0100

    Add android environment

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -6,3 +6,7 @@ alias update="zsh ~/.scripts/update.zsh"
 alias cleanupds="find . -type f -name '*.DS_Store' -ls -delete"
 
 alias pdf="${0:a:h}/print.sh"
+
+alias usenode10="export PATH=/usr/local/opt/node@10/bin:$PATH"
+
+alias usejava8="export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)"
