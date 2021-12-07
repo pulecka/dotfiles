1574d99647a36b97638d6e10e2005688e7686087
Martin Obert <martin@pulecka.dev>
2021-12-07 22:09:41 +0100

    Remove global Yarn installation

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
-# Setup XDG folders
-CONFIG_HOME=~/.config
-DATA_HOME=~/.local/share
+# Set up XDG folders
+XDG_CONFIG_HOME=~/.config
+XDG_CACHE_HOME=~/.cache
+XDG_DATA_HOME=~/.local/share
 
-mkdir -p $CONFIG_HOME
-mkdir -p $DATA_HOME
+mkdir -p $XDG_CONFIG_HOME
+mkdir -p $XDG_CACHE_HOME
+mkdir -p $XDG_DATA_HOME
 
 # Configure SSH
 mkdir -p ~/.ssh
@@ -28,13 +30,10 @@ xcode-select --install
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Install Brew packages
-brew install zsh git tmux neovim fzf ripgrep node yarn
+brew install zsh git tmux neovim fzf ripgrep node corepack
 
 # Install Brew apps
 brew install --cask 1password artpip cleanmymac firefox little-snitch parallels slack vlc
-
-# Install global NPM packages (mostly for LSP)
-yarn global add typescript typescript-language-server svelte-language-server
 
 # Checkout Dotfiles
 mkdir -p ~/Developer
@@ -42,39 +41,45 @@ cd ~/Developer
 git clone git@github.com:pulecka/dotfiles.git
 
 # Link Dotfiles
-mkdir -p $CONFIG_HOME/git
-ln -sv "dotfiles/git/config" $CONFIG_HOME/git/config
+mkdir -p $XDG_CONFIG_HOME/git
+ln -sv "dotfiles/git/config" $XDG_CONFIG_HOME/git/config
 
-mkdir -p $CONFIG_HOME/fish
-mkdir -p $CONFIG_HOME/fish/conf.d
-ln -sv "dotfiles/fish/config.fish" $CONFIG_HOME/fish/config.fish
-ln -sv "dotfiles/fish/functions" $CONFIG_HOME/fish/functions
-ln -sv "dotfiles/vagrant/vagrantvm.fish" $CONFIG_HOME/fish/conf.d/vagrantvm.fish
+mkdir -p $XDG_CONFIG_HOME/fish
+mkdir -p $XDG_CONFIG_HOME/fish/conf.d
+ln -sv "dotfiles/fish/config.fish" $XDG_CONFIG_HOME/fish/config.fish
+ln -sv "dotfiles/fish/functions" $XDG_CONFIG_HOME/fish/functions
+ln -sv "dotfiles/vagrant/vagrantvm.fish" $XDG_CONFIG_HOME/fish/conf.d/vagrantvm.fish
 
-mkdir -p $CONFIG_HOME/tmux
-ln -sv "dotfiles/tmux/tmux.conf" $CONFIG_HOME/tmux/tmux.conf
-ln -sv "dotfiles/tmux/project.tmux" $DATA_HOME/tmux/project.tmux
+mkdir -p $XDG_CONFIG_HOME/tmux
+ln -sv "dotfiles/tmux/tmux.conf" $XDG_CONFIG_HOME/tmux/tmux.conf
+ln -sv "dotfiles/tmux/project.tmux" $XDG_DATA_HOME/tmux/project.tmux
 
-ln -sv "dotfiles/nvim" $CONFIG_HOME/nvim
+ln -sv "dotfiles/nvim" $XDG_CONFIG_HOME/nvim
 
-# Install Neovim plugins
-curl -fLo ${DATA_HOME}/nvim/site/autoload/plug.vim --create-dirs \
+# Install Neovim plugins & Language servers
+NPM_CONFIG_USERCONFIG=$XDG_CONFIG_HOME/npm/config
+NPM_CONFIG_CACHE=$XDG_CACHE_HOME/npm
+
+npm install --global typescript typescript-language-server svelte-language-server
+
+curl -fLo $XDG_DATA_HOME/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-nvim --headless -u "${CONFIG_HOME}/nvim/lua/plugins.lua" -c "PlugInstall" -c "qall"
+nvim --headless -u "$XDG_CONFIG_HOME/nvim/lua/plugins.lua" -c "PlugInstall" -c "qall"
 
 # Switch to Fish shell
 chsh -s /usr/local/bin/fish
 
-set -U XDG_CONFIG_HOME $CONFIG_HOME
-set -U XDG_DATA_HOME $DATA_HOME
+set -U XDG_CONFIG_HOME $XDG_CONFIG_HOME
+set -U XDG_CACHE_HOME $XDG_CACHE_HOME
+set -U XDG_DATA_HOME $XDG_DATA_HOME
 
 # Install Dracula terminal theme
-mkdir -p $DATA_HOME/terminal
-curl -fLo $DATA_HOME/terminal/Dracula.terminal \
+mkdir -p $XDG_DATA_HOME/terminal
+curl -fLo $XDG_DATA_HOME/terminal/Dracula.terminal \
        https://raw.githubusercontent.com/dracula/terminal-app/master/Dracula.terminal
 
-curl -fLo $CONFIG_HOME/fish/conf.d/dracula.fish \
+curl -fLo $XDG_CONFIG_HOME/fish/conf.d/dracula.fish \
        https://raw.githubusercontent.com/dracula/fish/master/conf.d/dracula.fish
 
-open $DATA_HOME/terminal/Dracula.terminal
+open $XDG_DATA_HOME/terminal/Dracula.terminal
